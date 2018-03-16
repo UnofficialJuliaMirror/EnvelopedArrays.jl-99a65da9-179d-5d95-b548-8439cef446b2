ae0d40b288ba52f8b943c9154290c6e0ce072b27
Tyler Beason <tbeas12@gmail.com>
2018-03-16 14:00:44 -0700

    Added substance

--- src/EnvelopedArrays.jl
+++ src/EnvelopedArrays.jl
@@ -1,6 +1,67 @@
 __precompile__()
 module EnvelopedArrays
 
-# Package code goes here.
+import Base: tail
+export EnvelopedArray, parent, envelope
+
+
+struct EnvelopedArray{T<:Number,N,P} <: AbstractArray{T,N}
+   envelope::T
+   parent::P
+   size::NTuple{N,Int}
+   function EnvelopedArray{T,N,P}(envelope,parent,size) where {T,N,P}
+      new(envelope,parent,size)
+   end
+end
+function EnvelopedArray(envelope::T,parent::P) where {T<:Number,P}
+   psize = size(parent)
+   esize = (psize[1]+1,tail(psize)...)
+   EnvelopedArray{T,ndims(P),P}(envelope,parent,esize)
+end
+
+
+
+# accessors and utilities
+parent(A::EnvelopedArray) = A.parent
+envelope(A::EnvelopedArray) = A.envelope
+Base.size(A::EnvelopedArray) = A.size
+Base.similar(A::EnvelopedArray{T,N,P}) where {T,N,P} = EnvelopedArray(A.envelope,zeros(T,size(parent(A))))
+
+
+# Indexing functions
+function Base.getindex(A::EnvelopedArray{T,N,P}, I::Vararg{Int,N}) where {T,N,P}
+   if I[1] == 1
+      return envelope(A)
+   else
+      return getindex(parent(A),I[1]-1,tail(I)...)
+   end
+end
+
+function Base.getindex(A::EnvelopedArray{T,N,P}, i::Int) where {T,N,P}
+   @assert i >= 1
+   ci = ind2sub(size(A),i)
+   if i == 1 || ci[1] == 1
+      return envelope(A)
+   else
+      return getindex(parent(A),ci[1]-1,tail(ci)...)
+   end
+end
+
+Base.IndexStyle(::Type{<:EnvelopedArray}) = IndexCartesian()
+
+
+
+
+
+# for now, keep things immutable. to change envelope or parent, just create a new EnvelopedArray
+# function Base.setindex(A::EnvelopedArray{T,N,P},x,i::Int) where {T,N,P}
+   # @assert i >= 1
+   # ci = ind2sub(size(A),i)
+   # if i == 1 || ci[1] == 1
+      # error("Cannot mutate envelope.")
+   # else
+      # setindex!(parent(A),x,ci[1]-1,tail(ci)...)
+   # end
+# end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,14 @@ using EnvelopedArrays
 using Base.Test
 
 @testset "EnvelopedArrays.jl" begin
-    # Write your own tests here.
-    @test 1 == 2
+   # Write your own tests here.
+   a = [1.0 2.0 3.0; 4.0 5.0 6.0]
+   x = EnvelopedArray(1.0,a)
+   x_vcat = vcat(1.0,a)
+   @test all( x .== x_vcat)
+   @test x[1] == x_vcat[1]
+   @test x[2] == x_vcat[2]
+   @test x[end] == x_vcat[end]
+   @test x[2,2] == x_vcat[2,2]
+   @test x[1:3,1] .== x_vcat[1:3,1]
 end
